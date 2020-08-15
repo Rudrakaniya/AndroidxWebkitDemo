2ada2514ad6b8985b3333055abf3db802faf402f
Rudra kaniya <rudra.kaniya.rk@gmail.com>
2020-08-15 20:23:28 +0530

    Splash screen using launcher theme, working!!

--- app/src/main/java/com/example/androidxwebkitdemo/MainActivity.java
+++ app/src/main/java/com/example/androidxwebkitdemo/MainActivity.java
@@ -5,8 +5,12 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
 import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import java.util.Base64;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,15 +27,31 @@ public class MainActivity extends AppCompatActivity {
         mWebView.setWebChromeClient(new WebChromeClient());
         mWebView.setWebViewClient(new WebViewClient());
         mWebView.getSettings().setJavaScriptEnabled(true);
-        mWebView.loadUrl("http://gauravthakur.me/");
+
+//        mWebView.loadUrl("http://gauravthakur.me/");
+        String url = "<html> <body> <p>This is a paragraph.</p> <p>This is another paragraph.</p> <p><a href=\"https://www.google.com/\">Google</a></p> </body> </html>";
+        String loadURL = android.util.Base64.encodeToString(url.getBytes(), android.util.Base64.NO_PADDING);
+        mWebView.loadData(loadURL, "text/html", "base64");
+
+        mWebView.setWebViewClient(new LocalBrowser());
 
         mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
+                Toast.makeText(MainActivity.this, "on it",Toast.LENGTH_SHORT).show();
                 mWebView.reload();
                 mSwipeRefreshLayout.setRefreshing(false);
             }
         });
+
+    }
+    public class LocalBrowser extends WebViewClient{
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request){
+            String url = request.getUrl().toString();
+            view.loadUrl(url);
+            return true;
+        }
 
     }
 
